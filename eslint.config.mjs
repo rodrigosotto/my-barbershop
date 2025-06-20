03f889ecd1db7f24fa8683319e07743b91669074
jefferson.sotto <jeffersontads@hotmail.com>
2025-06-20 12:04:30 -0300

    🏗️ build(add files configs): create project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import nx from '@nx/eslint-plugin';
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  eslintPluginPrettierRecommended,
   {
     ignores: ['**/dist'],
   },
